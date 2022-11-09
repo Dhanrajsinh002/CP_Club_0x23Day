39cff550fc953d5b54f0a439457049d71cd5f5c7
Dhanrajsinh Parmar <79820599+Dhanrajsinh002@users.noreply.github.com>
2022-11-09 20:12:12 +0530

    Update 0x23_Day.c

--- 0x23_Day.c
+++ 0x23_Day.c
@@ -7,12 +7,10 @@
 
 int marks_summation(int* marks, int number_of_students, char gender) {
   //Write your code here.
-  int sum = 0, start;
+  int sum = 0, start = 1;
   
   if(gender == 'b') {
       start = 0;
-  } else {
-      start = 1;
   }
   
   for(int i = start; i < number_of_students; i = i +2) {
